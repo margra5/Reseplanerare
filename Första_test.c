9ecd2d09978fe930fbac2c04a0645937e0afd75e
Marcus Granberg <marcus@marcus-ThinkPad-X201.(none)>
2014-10-09 09:25:26 +0200

    som sagt var så är detta bara ett test

--- Första_test.c
+++ Första_test.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct{
+struct node {
   int x;
-  struct node * next;
-} node;
+  struct node *next;
+};
 
-int main(){
-  return 0;
+int main()
+{
+    /* Det här kommer att vara den orörliga första noden */
+    struct node *root;      
+
+    /* Nu pekar root till en node struct */
+    root = (struct node *) malloc( sizeof(struct node) ); 
+
+    /* noden roots har sin nästa pekare lika med en null pekare */
+    root->next = 0;  
+
+    /* Genom att använda operatorn ->, can man modifiera vad noden,
+       en pekare, pekar mot */
+    root->x = 5;     
 }
-
-
