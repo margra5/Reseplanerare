8ee3ee1387b063618778ff1d26b0c7157732e818
margra5 <marcus.granis@gmail.com>
2014-11-12 13:26:16 +0100

    KLAR

--- ExtraC.c
+++ ExtraC.c
@@ -10,40 +10,44 @@
 
 
 struct vara_t {
-  char namn[100];
-  char beskrivning[1000];
-  char plats_t[50];
+  char *namn;
+  char *beskrivning;
+  char *plats_t;
   int pris;
-  int antal;
-  struct vara *next;
+  int antal_1;
+  struct vara_t *next;
 };
 
 typedef struct db_t * DB;
 
 struct db_t{
   struct vara_t *first;
-  int antal;
+  int antal_2;
+  char last_used;
 };
 
-// Kolla upp 
-void add_to_db(DB db, struct vara_t * vara){
-  vara->next=db->first;
-  db->first=vara;
-  db->antal++;
-  printf("Info added to database\n");
-}
 
 // Lägg till
 
 
+void add_to_db(DB db, struct vara_t * vara){
+  vara -> next = db -> first;
+  db -> first = vara;
+  db -> antal_2++;
+  db -> last_used = 'l';
+  printf("Info added to database\n");
+}
+
 void print_item(struct vara_t *vara){
-  printf("Namn: %s\nAntal: %d\nBeskrivning: %s\nPlats: %s\nPris: %d",
+  printf("          ========================================\n");
+  printf("           %s\n           %d\n           %s\n           %s\n           %d\n",
 	 vara -> namn,
-	 vara -> antal,
+	 vara -> antal_1,
 	 vara -> beskrivning,
 	 vara -> plats_t,
 	 vara -> pris);
-}
+  printf("          ========================================\n");
+};
 
 char ask_char_question(char * q, char * a){
   printf("%s [%s]\n",q,a);
@@ -57,20 +61,22 @@ char ask_char_question(char * q, char * a){
   }
 }
 
-char ask_string_question(char * c){
-  char b[500];  
+char * ask_string_question(char * c){
+  char* b = malloc(sizeof(char) * 500);
   printf("Nämn %s: ", c);
   scanf("%[^\n]", b);
   while(getchar()!='\n');
+  return b;
 }
 
 void add_item(DB db){
   struct vara_t *vara = malloc(sizeof(struct vara_t));
-  vara->namn = ask_string_question("Namn");
-  vara->antal = ask_string_question("Antal");
-  vara->beskrivning = ask_string_question("Beskrivning");
-  vara->plats_t = ask_string_question("Plats");
-  vara->pris = ask_string_question("Pris");
+  vara -> next = NULL;
+  vara -> namn = ask_string_question("Namn");
+  vara -> antal_1 = atoi(ask_string_question("Antal"));
+  vara -> beskrivning = ask_string_question("Beskrivning");
+  vara -> plats_t = ask_string_question("Plats");
+  vara -> pris = atoi(ask_string_question("Pris"));
   print_item(vara);
   if(ask_char_question("\nSpara till Db?", "YyNn") == 'y'){
     add_to_db(db, vara);
@@ -79,19 +85,60 @@ void add_item(DB db){
   }
 }
 
-
-
 // Ta bort 
 
-// Ångra vara 
+// Ångra vara
+
+void undo(DB db_1, DB db_2){
+  if (db_1 -> last_used == 'l'){
+    printf("Senaste tilllagda varan har ångrats\n");
+    db_1 -> first = db_1 -> first -> next;
+    db_1 -> antal_2--;
+    db_1 -> last_used = 'o';
+  }
+  /*
+    else if(db_1 -> last_used == 'm'){
+    printf("Ångra att du tog bort senast tillagd vara\n");
+    db_2 -> first = db_1 -> first;
+    db_1 -> antal_2++;
+    db_1 -> last_used = 'l';
+    }
+  */
+  else if (db_1 -> last_used == 'r'){
+    printf("Senaste redigerade varan har ångrats\n");
+    db_1 -> last_used = 'o';
+  }
+  else if (db_1 -> last_used == 't'){
+    printf("Senaste borttagna varan har ångrats\n");
+    db_1 -> last_used = 'o';
+  } 
+  else if (db_1 -> last_used == 'o'){
+    printf("Det finns inget att ångra\n");
+  }
+  else {
+    printf("last_used är satt till en odef char");
+  } 
+}
+
 
 // lagersaldo
+
+void print_all(DB db){
+  int i;
+  struct vara_t *vara = db -> first;
+  if (db -> first == NULL){
+    printf("Du har inga varor i databasen\n");
+  }else{
+    for(i = 0; i < db -> antal_2; i++){
+      print_item(vara);
+      vara = vara -> next;
+    }
+  }
+}
 
 // redigera vara 
 
 // Avsluta
-
-
 
 
 void print_main_menu(void){
@@ -106,21 +153,25 @@ void print_main_menu(void){
   puts("Please choose an operation");
   puts("");
   puts("A or a. Avsluta programet");
-  puts("Z or z. Ångra vara (Ej implementerat");
-  puts("L or l. Lägg till vara (Ej klart)");
+  puts("Z or z. Ångra vara (Inte helt klar)");
+  puts("L or l. Lägg till vara");
   puts("T or t. Ta bort vara (Ej implementerat)");
   puts("R or r. Redigera vara (Ej implementerat)");
-  puts("I or i. Skriv ut alla varor? (Ej implementerat)");
+  puts("I or i. Skriv ut alla varor?");
 }
 
 int main(){
+  //vara_t vara = malloc(sizeof(struct vara_t));
   DB TEST = malloc(sizeof(struct db_t));
+  DB Undo_db = malloc(sizeof(struct db_t));
   TEST -> first = NULL;
-  TEST -> antal = 0;
+  TEST -> antal_2 = 0;
+  TEST -> last_used = 'o';
+
   while(true){
     print_main_menu();
 
-    switch(ask_char_question("\nVad vill du göra idag?", "AaZzLlTtRrIi")){
+    switch(ask_char_question("\nVad vill du göra idag?", "AaZzLlTtRrIiQq")){
 
       // case 'a': Avsluta
       // Vid ingång i case 6 så ska programet fråga, Vill du avsluta
@@ -136,7 +187,7 @@ int main(){
       // case 3: Ångra vara 
 
     case 'z':
-      printf("Du har inte implementerat Ångra\n");
+      undo(TEST, Undo_db);
       break;
 
       //  case 1: Lägg till vara 
@@ -166,15 +217,14 @@ int main(){
     case 'r':
       printf("Du har inte implementerat redigera\n");
       break;
-
+      
       //  case 4: Printa lagersaldo (för specifik vara eller alla?) */
-
+      
     case 'i':
-      printf("du har inte implementerat listning av varor\n");
+      print_all(TEST);
       break;
-    }
-      
-      
+
+    }   
   }
   return 0;
 }
